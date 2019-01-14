ca979728193e27e83554a85dab4eb1ac8ff2e5a8
Pundlik <pundlik.sarafdar@hp.com>
2019-01-14 08:45:23 +0530

    dependancies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 express = require('express');
 bodyParser = require('body-parser');
 server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 routes = require("./api/routes/routes") ;
 server = express(),
   
